502c04fc171f3b5cc31cdc5ef17dd4d3f6c3eff6
Ryan Erricson <ryanerricson@gmail.com>
2021-03-15 10:11:29 -0400

    commit tree 763fa1b231f2234bfe5435c7f431989ff3750d46

---
+++ app/models/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+    @@all = []
+
+    attr_accessor :name, :age, :breed
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
